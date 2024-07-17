c71f438243dced34914930732a4fcb6d274b7b6d
Jiho <yjh7827@naver.com>
2024-07-18 00:07:29 +0900

    feat: initial commit

---
+++ Practice_Widget/App/Practice_WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_WidgetApp.swift
+//  Practice_Widget
+//
+//  Created by 유지호 on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Practice_WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
